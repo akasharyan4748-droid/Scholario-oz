b28f7bc1387d32507cd7adeef260a3a858709561
Z User <z@container>
2026-08-21 08:59:11 +0000

    1323bfb4-6408-4071-8a42-79139b6d308d

--- start-dev.sh
+++ start-dev.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Auto-restart dev server with signal trapping
+trap '' SIGHUP SIGTERM
 cd /home/z/my-project
 while true; do
   env NODE_OPTIONS="--max-old-space-size=256" bun run dev >> dev.log 2>&1 &
   PID=$!
-  wait $PID
+  wait $PID 2>/dev/null
+  echo "[watchdog] Server died at $(date), restarting in 2s..." >> dev.log
   sleep 2
 done
